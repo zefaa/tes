7d01112ce0247bca86bd1adf15dca110db6a971a
imavi3 <imavi3@icloud.com>
2023-08-23 14:36:45 +0700

    23-08(6)

--- components/TeamOne.vue
+++ components/TeamOne.vue
@@ -4,9 +4,7 @@
       <!-- Section Title Start -->
       <div class="section-title text-center mb-60" data-aos="fade-up">
         <h2 class="title">Tim Yayasan</h2>
-        <p class="sub-title">
-          Yohannes Gabriel
-        </p>
+        <p class="sub-title">Yohannes Gabriel</p>
       </div>
       <!-- Section Title End -->
 
@@ -27,7 +25,6 @@
 </template>
 
 <script>
-
 export default {
   components: {
     TeamMember: () => import("@/components/TeamMember"),
@@ -62,228 +59,221 @@ export default {
       },
 
       teamData: [
-       // {
-        //   id: 7,
-        //   imgSrc: "/images/team/ketum.jpg",
-        //   name: "Romo Tommy",
-        //   position: "Ketua Umum Yayasan",
-        // },
-        // {
-        //   id: 8,
-        //   imgSrc: "/images/team/kabid-kepd.jpg",
-        //   name: "Romo Karel",
-        //   position: "Ketua Bidang Kependidikan-Katolisitas",
-        // },
-        // {
-        //   id: 9,
-        //   imgSrc: "/images/team/kabid-keu.jpg",
-        //   name: "Romo Hans",
-        //   position: "Ketua Bidang Keuangan",
-        // },
-        // {
-        //   id: 10,
-        //   imgSrc: "/images/team/kabid-legal.jpg",
-        //   name: "Romo Domi",
-        //   position: "Ketua Bidang Legal-Kerjasama",
-        // },
         {
-          id: 11,
+          id: 1,
+          imgSrc: "/images/team/ketum.png",
+          name: "Romo Tommy",
+          position: "Ketua Umum Yayasan",
+        },
+        {
+          id: 2,
+          imgSrc: "/images/team/sekre.png",
+          name: "Romo Theo",
+          position: "Sekretaris Yayasan",
+        },
+        {
+          id: 3,
+          imgSrc: "/images/team/bend.png",
+          name: "Romo Rudi",
+          position: "Bendahara Yayasan",
+        },
+        {
+          id: 4,
+          imgSrc: "/images/team/kabid-kepn.png",
+          name: "Romo Karel",
+          position: "Ketua Bidang Kependidikan-Katolisitas",
+        },
+        {
+          id: 5,
+          imgSrc: "/images/team/kabid-keu.png",
+          name: "Romo Hans",
+          position: "Ketua Bidang Keuangan",
+        },
+        {
+          id: 6,
+          imgSrc: "/images/team/kabid-legal.png",
+          name: "Romo Domi",
+          position: "Ketua Bidang Legal-Kerjasama",
+        },
+        {
+          id: 7,
           imgSrc: "/images/team/kabid-sarpras.png",
           name: "Romo Novi",
           position: "Ketua Bidang Sarana dan Prasarana",
         },
         {
-          id: 12,
+          id: 8,
           imgSrc: "/images/team/kabid-sdm.png",
           name: "Romo Udit",
           position: "Ketua Bidang Sumber Daya Manusia",
         },
-        // {
-        //   id: 13,
-        //   imgSrc: "/images/team/bend.jpg",
-        //   name: "Romo Rudi",
-        //   position: "Bendahara Yayasan",
-        // },
-                {
-          id: 15,
+
+        {
+          id: 9,
           imgSrc: "/images/team/team-lilis.png",
           name: "Lilis Lucia",
           position: "Kependidikan-Katolisitas",
         },
         {
-          id: 25,
+          id: 10,
           imgSrc: "/images/team/team-sari.png",
           name: "Maria Ratna Rosari",
           position: "Kependidikan dan Katolisitas",
         },
         {
-          id: 2,
+          id: 11,
           imgSrc: "/images/team/team-ayu.png",
           name: "Caroline Ayu",
           position: "Kependidikan-Katolisitas",
         },
-         {
-          id: 19,
+        {
+          id: 12,
           imgSrc: "/images/team/team-olfa.png",
           name: "Maria Olfa",
           position: "Kependidikan-Katolisitas",
         },
         {
-          id: 18,
+          id: 13,
           imgSrc: "/images/team/team-markus.png",
           name: "Markus ",
           position: "Legal-Kerjasama",
         },
         {
-          id: 5,
+          id: 14,
           imgSrc: "/images/team/team-eric.png",
           name: "Eric Leon",
           position: "Legal-Kerjasama",
         },
- {
-          id: 4,
+        {
+          id: 15,
           imgSrc: "/images/team/team-flo.png",
           name: "Florensia Dista",
           position: "Legal-Kerjasama",
         },
         {
-          id: 23,
+          id: 16,
           imgSrc: "/images/team/team-rere.png",
           name: "Regina Martha",
           position: "SDM",
         },
         {
-          id: 34,
+          id: 17,
           imgSrc: "/images/team/team-valeria.png",
           name: "Valeria Kartikaningtyas",
           position: "SDM",
         },
         {
-          id: 33,
+          id: 18,
           imgSrc: "/images/team/team-heru.png",
           name: "Aloysius Heru",
           position: "SDM",
         },
-         {
-          id: 14,
+        {
+          id: 19,
           imgSrc: "/images/team/team-gun.png",
           name: "Fredy Gunawan",
           position: "SDM",
         },
         {
-          id: 21,
+          id: 20,
           imgSrc: "/images/team/team-putri.png",
           name: "Putri Prihantini",
           position: "Sarana dan Prasarana",
         },
         {
-          id: 32,
+          id: 21,
           imgSrc: "/images/team/team-wisnu.png",
           name: "Yohanes Wisnu",
           position: "Keuangan",
         },
         {
-          id: 30,
+          id: 22,
           imgSrc: "/images/team/team-tri.png",
           name: "Margaretha Tri",
           position: "Keuangan",
         },
-         {
-          id: 1,
+        {
+          id: 23,
           imgSrc: "/images/team/team-arta.png",
           name: "Arta",
           position: "Accounting",
         },
-         {
-          id: 16,
+        {
+          id: 24,
           imgSrc: "/images/team/team-louren.png",
           name: "Lourensia Ratri",
           position: "Accounting",
         },
-          {
-          id: 26,
+        {
+          id: 25,
           imgSrc: "/images/team/team-sthevani.png",
           name: "Sthevani Surya",
           position: "Accounting",
         },
         {
-          id: 20,
+          id: 26,
           imgSrc: "/images/team/team-paula.png",
           name: "Paula Dian",
           position: "Accounting",
         },
-       
-           {
-          id: 28,
+
+        {
+          id: 27,
           imgSrc: "/images/team/team-tanti.png",
           name: "Theresia Kristanti",
           position: "Accounting",
         },
         {
-          id: 17,
+          id: 28,
           imgSrc: "/images/team/team-maria.png",
           name: "Maria Tanda Agung",
           position: "Umum",
         },
-         {
+        {
           id: 29,
           imgSrc: "/images/team/team-tata.png",
           name: "Brigietta Irna",
           position: "Umum",
         },
-        
+
         {
-          id: 31,
+          id: 30,
           imgSrc: "/images/team/team-ve.png",
           name: "Zefanya Devendra",
           position: "Umum",
         },
-         {
-          id: 3,
+        {
+          id: 31,
           imgSrc: "/images/team/team-dian.png",
           name: "Patrianus Dikson",
           position: "Umum",
         },
         {
-          id: 6,
+          id: 32,
           imgSrc: "/images/team/team-goris.png",
           name: "Gregorius Najok",
           position: "RGU",
         },
         {
-          id: 22,
+          id: 33,
           imgSrc: "/images/team/team-rafael.png",
           name: "Rafael Kaflo",
           position: "RGU",
         },
-        
+
         {
-          id: 24,
+          id: 34,
           imgSrc: "/images/team/team-richard.png",
           name: "Richard",
           position: "RGU",
         },
-        
-      
+
         {
-          id: 27,
+          id: 35,
           imgSrc: "/images/team/team-sugeng.png",
           name: "Albertus Sugeng",
           position: "RGU",
         },
-        
-       
-
-       
-        
-        
-       
-        
-        
-        
-      
-        
-        
       ],
     };
   },
